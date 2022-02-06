1e48a0b2e60529fcecf6c5b767a66e33862d958f
Usame Yiğit <erusame@hotmail.com>
2022-02-06 03:03:03 +0300

    Add project files.

---
+++ ArrayClassStudiesOptimised/ArrayClassMethods.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace ArrayClassStudiesOptimised
+{
+    partial class ArrayClassMethods
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSortArray = new System.Windows.Forms.Button();
+            this.btnResetArray = new System.Windows.Forms.Button();
+            this.lsChanged = new System.Windows.Forms.ListBox();
+            this.lsOriginal = new System.Windows.Forms.ListBox();
+            this.btnResiezeArray = new System.Windows.Forms.Button();
+            this.btnLastIndexOfArray = new System.Windows.Forms.Button();
+            this.btnIndexOfArray = new System.Windows.Forms.Button();
+            this.btnCopyArray = new System.Windows.Forms.Button();
+            this.btnClearArray = new System.Windows.Forms.Button();
+            this.btnReverseArray = new System.Windows.Forms.Button();
+            this.btnMoreThanOne = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSortArray
+            // 
+            this.btnSortArray.Location = new System.Drawing.Point(45, 330);
+            this.btnSortArray.Name = "btnSortArray";
+            this.btnSortArray.Size = new System.Drawing.Size(216, 45);
+            this.btnSortArray.TabIndex = 19;
+            this.btnSortArray.Text = "Array.Sort()";
+            this.btnSortArray.UseVisualStyleBackColor = true;
+            this.btnSortArray.Click += new System.EventHandler(this.btnSortArray_Click);
+            // 
+            // btnResetArray
+            // 
+            this.btnResetArray.Location = new System.Drawing.Point(45, 433);
+            this.btnResetArray.Name = "btnResetArray";
+            this.btnResetArray.Size = new System.Drawing.Size(216, 45);
+            this.btnResetArray.TabIndex = 18;
+            this.btnResetArray.Text = "Reset Array";
+            this.btnResetArray.UseVisualStyleBackColor = true;
+            this.btnResetArray.Click += new System.EventHandler(this.btnResetArray_Click);
+            // 
+            // lsChanged
+            // 
+            this.lsChanged.FormattingEnabled = true;
+            this.lsChanged.Location = new System.Drawing.Point(546, 24);
+            this.lsChanged.Name = "lsChanged";
+            this.lsChanged.Size = new System.Drawing.Size(209, 407);
+            this.lsChanged.TabIndex = 17;
+            // 
+            // lsOriginal
+            // 
+            this.lsOriginal.FormattingEnabled = true;
+            this.lsOriginal.Location = new System.Drawing.Point(299, 24);
+            this.lsOriginal.Name = "lsOriginal";
+            this.lsOriginal.Size = new System.Drawing.Size(209, 407);
+            this.lsOriginal.TabIndex = 16;
+            // 
+            // btnResiezeArray
+            // 
+            this.btnResiezeArray.Location = new System.Drawing.Point(45, 228);
+            this.btnResiezeArray.Name = "btnResiezeArray";
+            this.btnResiezeArray.Size = new System.Drawing.Size(216, 45);
+            this.btnResiezeArray.TabIndex = 15;
+            this.btnResiezeArray.Text = "Array.Resieze()";
+            this.btnResiezeArray.UseVisualStyleBackColor = true;
+            this.btnResiezeArray.Click += new System.EventHandler(this.btnResiezeArray_Click);
+            // 
+            // btnLastIndexOfArray
+            // 
+            this.btnLastIndexOfArray.Location = new System.Drawing.Point(45, 177);
+            this.btnLastIndexOfArray.Name = "btnLastIndexOfArray";
+            this.btnLastIndexOfArray.Size = new System.Drawing.Size(216, 45);
+            this.btnLastIndexOfArray.TabIndex = 14;
+            this.btnLastIndexOfArray.Text = "Array.LastIndexOf()";
+            this.btnLastIndexOfArray.UseVisualStyleBackColor = true;
+            this.btnLastIndexOfArray.Click += new System.EventHandler(this.btnLastIndexOfArray_Click);
+            // 
+            // btnIndexOfArray
+            // 
+            this.btnIndexOfArray.Location = new System.Drawing.Point(45, 126);
+            this.btnIndexOfArray.Name = "btnIndexOfArray";
+            this.btnIndexOfArray.Size = new System.Drawing.Size(216, 45);
+            this.btnIndexOfArray.TabIndex = 13;
+            this.btnIndexOfArray.Text = "Array.IndexOf()";
+            this.btnIndexOfArray.UseVisualStyleBackColor = true;
+            this.btnIndexOfArray.Click += new System.EventHandler(this.btnIndexOfArray_Click);
+            // 
+            // btnCopyArray
+            // 
+            this.btnCopyArray.Location = new System.Drawing.Point(45, 75);
+            this.btnCopyArray.Name = "btnCopyArray";
+            this.btnCopyArray.Size = new System.Drawing.Size(216, 45);
+            this.btnCopyArray.TabIndex = 12;
+            this.btnCopyArray.Text = "Array.Copy()";
+            this.btnCopyArray.UseVisualStyleBackColor = true;
+            this.btnCopyArray.Click += new System.EventHandler(this.btnCopyArray_Click);
+            // 
+            // btnClearArray
+            // 
+            this.btnClearArray.Location = new System.Drawing.Point(45, 24);
+            this.btnClearArray.Name = "btnClearArray";
+            this.btnClearArray.Size = new System.Drawing.Size(216, 45);
+            this.btnClearArray.TabIndex = 11;
+            this.btnClearArray.Text = "Array.Clear()";
+            this.btnClearArray.UseVisualStyleBackColor = true;
+            this.btnClearArray.Click += new System.EventHandler(this.btnClearArray_Click);
+            // 
+            // btnReverseArray
+            // 
+            this.btnReverseArray.Location = new System.Drawing.Point(45, 279);
+            this.btnReverseArray.Name = "btnReverseArray";
+            this.btnReverseArray.Size = new System.Drawing.Size(216, 45);
+            this.btnReverseArray.TabIndex = 10;
+            this.btnReverseArray.Text = "Array.Reverse()";
+            this.btnReverseArray.UseVisualStyleBackColor = true;
+            this.btnReverseArray.Click += new System.EventHandler(this.btnReverseArray_Click);
+            // 
+            // btnMoreThanOne
+            // 
+            this.btnMoreThanOne.Location = new System.Drawing.Point(45, 381);
+            this.btnMoreThanOne.Name = "btnMoreThanOne";
+            this.btnMoreThanOne.Size = new System.Drawing.Size(216, 45);
+            this.btnMoreThanOne.TabIndex = 20;
+            this.btnMoreThanOne.Text = "Find More Than One";
+            this.btnMoreThanOne.UseVisualStyleBackColor = true;
+            this.btnMoreThanOne.Click += new System.EventHandler(this.btnMoreThanOne_Click);
+            // 
+            // ArrayClassMethods
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.btnMoreThanOne);
+            this.Controls.Add(this.btnSortArray);
+            this.Controls.Add(this.btnResetArray);
+            this.Controls.Add(this.lsChanged);
+            this.Controls.Add(this.lsOriginal);
+            this.Controls.Add(this.btnResiezeArray);
+            this.Controls.Add(this.btnLastIndexOfArray);
+            this.Controls.Add(this.btnIndexOfArray);
+            this.Controls.Add(this.btnCopyArray);
+            this.Controls.Add(this.btnClearArray);
+            this.Controls.Add(this.btnReverseArray);
+            this.Name = "ArrayClassMethods";
+            this.Text = "Array Class Methods With Methods";
+            this.Load += new System.EventHandler(this.ArrayClassMethods_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSortArray;
+        private System.Windows.Forms.Button btnResetArray;
+        private System.Windows.Forms.ListBox lsChanged;
+        private System.Windows.Forms.ListBox lsOriginal;
+        private System.Windows.Forms.Button btnResiezeArray;
+        private System.Windows.Forms.Button btnLastIndexOfArray;
+        private System.Windows.Forms.Button btnIndexOfArray;
+        private System.Windows.Forms.Button btnCopyArray;
+        private System.Windows.Forms.Button btnClearArray;
+        private System.Windows.Forms.Button btnReverseArray;
+        private System.Windows.Forms.Button btnMoreThanOne;
+    }
+}
+
---
+++ ArrayClassStudiesOptimised/ArrayClassMethods.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayClassStudiesOptimised
+{
+    public partial class ArrayClassMethods : Form
+    {
+        string[] cities = { "Adana", "İzmir", "İstanbul", "Ankara", "Giresun", "Trabzon", "Rize", "İstanbul", "Samsun", "Newşehir" };
+        string[] copyCities = new string[8];
+
+        public ArrayClassMethods()
+        {
+            InitializeComponent();
+        }
+
+
+        #region On Load
+        private void ArrayClassMethods_Load(object sender, EventArgs e)
+        {
+            PrintToListBox(lsOriginal, cities, false);
+        }
+        #endregion
+
+        #region Button Click Events
+        private void btnClearArray_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Array.Clear(cities, 2, 4);
+            }
+            #region Exceptions
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show(ane.Message);
+            }
+            catch (IndexOutOfRangeException ioore)
+            {
+                MessageBox.Show(ioore.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+
+            PrintToListBox(lsChanged, cities, false);
+
+        }
+
+        private void btnCopyArray_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Array.Copy(cities, 5, copyCities, 2, 4);
+                //Array.Copy(cities, copyCities, 4);
+            }
+            #region Exceptions
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show(ane.Message);
+            }
+            catch (RankException re)
+            {
+                MessageBox.Show(re.Message);
+            }
+            catch (ArrayTypeMismatchException atme)
+            {
+                MessageBox.Show(atme.Message);
+            }
+            catch (InvalidCastException ice)
+            {
+                MessageBox.Show(ice.Message);
+            }
+            catch (ArgumentOutOfRangeException aoore)
+            {
+                MessageBox.Show(aoore.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                MessageBox.Show(ae.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+
+            PrintToListBox(lsChanged, copyCities, false);
+        }
+
+        private void btnLastIndexOfArray_Click(object sender, EventArgs e)
+        {
+            ClearItemLists(lsChanged);
+
+            try
+            {
+                lsChanged.Items.Add(Array.LastIndexOf(cities, "İstanbul"));
+                lsChanged.Items.Add(Array.LastIndexOf(cities, "İstanbul", 6));
+            }
+            #region Exceptions
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show(ane.Message);
+            }
+            catch (ArgumentOutOfRangeException aoore)
+            {
+                MessageBox.Show(aoore.Message);
+            }
+            catch (SystemException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+
+        }
+
+        private void btnIndexOfArray_Click(object sender, EventArgs e)
+        {
+            ClearItemLists(lsChanged);
+
+            try
+            {
+                lsChanged.Items.Add(Array.IndexOf(cities, "İstanbul"));
+                lsChanged.Items.Add(Array.IndexOf(cities, "İstanbul", 3));
+            }
+            #region Exceptions
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show(ane.Message);
+            }
+            catch (ArgumentOutOfRangeException aoore)
+            {
+                MessageBox.Show(aoore.Message);
+            }
+            catch (SystemException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+
+        }
+
+        private void btnResiezeArray_Click(object sender, EventArgs e)
+        {
+            ClearItemLists(lsChanged);
+
+            try
+            {
+                Array.Resize<string>(ref cities, 4);
+                Array.Resize<string>(ref cities, 12);
+            }
+            #region Exceptions
+            catch (ArgumentOutOfRangeException aoore)
+            {
+
+                MessageBox.Show(aoore.Message);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            #endregion
+
+            PrintToListBox(lsChanged, cities, false);
+
+        }
+
+        private void btnReverseArray_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Array.Reverse(cities);
+            }
+            #region Exceptions
+            catch (ArgumentNullException ane)
+            {
+
+                MessageBox.Show(ane.Message);
+            }
+            catch (RankException re)
+            {
+
+                MessageBox.Show(re.Message);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+
+            PrintToListBox(lsChanged, cities, false);
+
+        }
+
+        private void btnSortArray_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Array.Sort(cities);
+            }
+            #region Exceptions
+            catch (ArgumentNullException ane)
+            {
+
+                MessageBox.Show(ane.Message);
+            }
+            catch (InvalidOperationException ioe)
+            {
+
+                MessageBox.Show(ioe.Message);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+            PrintToListBox(lsChanged, cities, false);
+
+        }
+
+        private void btnResetArray_Click(object sender, EventArgs e)
+        {
+            cities = new string[10] { "Adana", "İzmir", "İstanbul", "Ankara", "Giresun", "Trabzon", "Rize", "İstanbul", "Samsun", "Newşehir" };
+
+            PrintToListBox(lsChanged, cities, false);
+
+        }
+
+        private void btnMoreThanOne_Click(object sender, EventArgs e)
+        {
+
+            string search = "Newşehir";
+            PrintMoreThanOne(cities, search, "city");
+            search = "İstanbul";
+            PrintMoreThanOne(cities, search, "city");
+            search = "Berlin";
+            PrintMoreThanOne(cities, search, "city");
+
+        }
+        #endregion
+
+        #region Methods
+        void PrintToListBox(ListBox lsToPrint, string[] valuesPrint, bool showMessageBox)
+        {
+            ClearItemLists(lsToPrint);
+
+            try
+            {
+                foreach (string value in valuesPrint)
+                {
+                    if(showMessageBox)
+                        MessageBox.Show(value);
+                    if (value != null)
+                    {
+                        lsToPrint.Items.Add(value);
+                    }
+                    else
+                    {
+                        lsToPrint.Items.Add("null");
+                    }
+                }
+            }
+            #region Exceptions
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show(ane.Message);
+            }
+            catch (SystemException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+        }
+
+        bool IsMoreThanOne(string[] values, string search, out bool isThereCity)
+        {
+            try
+            {
+                int count = 0;
+                foreach (string value in values)
+                {
+                    if (value == search)
+                        count++;
+                }
+
+                isThereCity = !(count <= 0);
+                return (count > 1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                isThereCity = false;
+                return false;
+            }  
+        }
+
+        void PrintMoreThanOne(string[] values, string search, string nameOfArray)
+        {
+            try
+            {
+                bool isThere = false;
+
+                if (IsMoreThanOne(values, search, out isThere))
+                {
+                    MessageBox.Show("There are more than one " + nameOfArray + " named as: " + search + " in the list.");
+                }
+                else
+                {
+                    if (isThere)
+                    {
+                        MessageBox.Show("There is only one " + nameOfArray + " named such: " + search + " in the list.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("There is no " + nameOfArray + " named such: " + search + " in the list.");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        void ClearItemLists(ListBox lsToClear)
+        {
+            lsToClear.Items.Clear();
+        }
+        #endregion
+
+        
+    }
+}
